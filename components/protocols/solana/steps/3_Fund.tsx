e75e6e04c4ec22df1d12a7233036a008a9909594
beastrc <makarov0114@outlook.com>
2021-07-13 17:06:39 +0200

    rewrite [chain].tsx leveraging nextjs features

---
+++ components/protocols/solana/steps/3_Fund.tsx
@@ -0,0 +1,62 @@
+import { useState } from "react"
+import { Alert, Button, Space, Col, Input, Typography } from 'antd'
+import axios from "axios"
+import { SolanaFundReponse, SolanaTransferErrorResponse } from "types/solana-types"
+
+const { Text } = Typography
+
+const Fund = () => {
+  const [value, setValue] = useState<string | null>("")
+  const [fetching, setFetching] = useState<boolean>(false);
+  const [error, setError] = useState<string | null>(null);
+  const [isFunded, setIsFunded] = useState<boolean>(false)
+
+  const fund = () => {
+    if (!value) return null
+
+    setFetching(true)
+		axios
+			.post(
+        `${process.env.NEXT_PUBLIC_SERVER_URL}/api/solana/fund`,
+        {
+					address: value,
+				},
+      )
+			.then(res => {
+				const data: SolanaFundReponse = res.data
+        console.log(`data`, data)
+				setIsFunded(true)
+				setFetching(false)
+			})
+			.catch(err => {
+				console.log(`err.response`, err.response)
+				const data: SolanaTransferErrorResponse = err.response.data
+				setFetching(false)
+				setError(data.message)
+			})
+  }
+  
+  return (
+    <Col>
+      <Space direction="vertical" size="large">
+        <Space direction="vertical">
+          <Text>Paste the address you generated (you can copy it in the top right corner of the page):</Text>
+          <Input placeholder="Enter an address" onChange={(e) => setValue(e.target.value) } style={{ width: "500px" }} />
+          <Button type="primary" onClick={fund}>Fund this address</Button>
+        </Space>
+        {error &&
+          <Alert
+            type="error"
+            showIcon
+            closable
+            message={error}
+            onClose={() => setError(null)}
+          />
+        }
+        {isFunded && <Alert message={<Text strong>Address Funded!</Text>} type="success" showIcon />}
+      </Space>
+    </Col>
+  )
+}
+
+export default Fund
--- types/solana-types.ts
+++ types/solana-types.ts
@@ -1,3 +1,13 @@
 import { Version } from "@solana/web3.js";
 
 export type SolanaConnectReponse = Version
+export type SolanaFundReponse = string
+
+export type SolanaTransferErrorResponse = {
+	message: string
+}
+
+export type SolanaBalanceReponse = number
+export type SolanaBalanceErrorResponse = {
+  message: string
+}
