572c4ad063a85c679ca36f9f6408b02710d85529
beastrc <makarov0114@outlook.com>
2021-07-20 16:32:45 -0700

    fix comments

--- contracts/polygon/SimpleStorage/truffle-config.js
+++ contracts/polygon/SimpleStorage/truffle-config.js
@@ -19,20 +19,20 @@ module.exports = {
      host: "127.0.0.1",
      port: 9545,
      network_id: "*"
-   },
-  //  matic:  {
-  //     provider: () => new HDWalletProvider({
-  //       mnemonic: {
-  //         phrase: mnemonic
-  //       },
-  //       providerOrUrl:  `https://matic-mumbai.chainstacklabs.com`,
-  //       chainId: 80001
-  //     }),
-  //     network_id: 80001,
-  //     confirmations: 2,
-  //     timeoutBlocks: 200,
-  //     skipDryRun: true,
-  //     chainId: 80001
-  //   }
-  // }
+   }//,
+   // matic:  {
+   //    provider: () => new HDWalletProvider({
+   //      mnemonic: {
+   //        phrase: mnemonic
+   //      },
+   //      providerOrUrl:  `https://matic-mumbai.chainstacklabs.com`,
+   //      chainId: 80001
+   //    }),
+   //    network_id: 80001,
+   //    confirmations: 2,
+   //    timeoutBlocks: 200,
+   //    skipDryRun: true,
+   //    chainId: 80001
+   //  }
+  }
 };
