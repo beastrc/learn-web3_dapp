0c4f1d60b0a272f965877359178075c1051c353a
beastrc <makarov0114@outlook.com>
2021-07-20 16:33:28 -0700

    remove some default comments that may confuse people

--- contracts/polygon/SimpleStorage/truffle-config.js
+++ contracts/polygon/SimpleStorage/truffle-config.js
@@ -3,12 +3,9 @@
 // const mnemonic = fs.readFileSync(".secret").toString().trim();
 
 module.exports = {
-  // Uncommenting the defaults below
-  // provides for an easier quick-start with Ganache.
-  // You can also follow this format for other networks;
-  // see <http://truffleframework.com/docs/advanced/configuration>
+  // See <http://truffleframework.com/docs/advanced/configuration>
   // for more details on how to specify configuration options!
-  //
+
   networks: {
    development: {
      host: "127.0.0.1",
