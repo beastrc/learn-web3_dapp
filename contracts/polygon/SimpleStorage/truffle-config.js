6b74139e9458e6cb3b59e1cd2ea36c572102ad15
beastrc <makarov0114@outlook.com>
2021-07-28 00:50:49 -0600

    re-enable matic in truffle-config

--- contracts/polygon/SimpleStorage/truffle-config.js
+++ contracts/polygon/SimpleStorage/truffle-config.js
@@ -1,6 +1,6 @@
-// const HDWalletProvider = require('@truffle/hdwallet-provider');
-// const fs = require('fs');
-// const mnemonic = fs.readFileSync(".secret").toString().trim();
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const fs = require('fs');
+const mnemonic = fs.readFileSync(".secret").toString().trim();
 
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
@@ -28,20 +28,20 @@ module.exports = {
      host: "127.0.0.1",
      port: 9545,
      network_id: "*"
-   }//,
-   // matic:  {
-   //    provider: () => new HDWalletProvider({
-   //      mnemonic: {
-   //        phrase: mnemonic
-   //      },
-   //      providerOrUrl:  `https://matic-mumbai.chainstacklabs.com`,
-   //      chainId: 80001
-   //    }),
-   //    network_id: 80001,
-   //    confirmations: 2,
-   //    timeoutBlocks: 200,
-   //    skipDryRun: true,
-   //    chainId: 80001
-   //  }
+   },
+   matic:  {
+      provider: () => new HDWalletProvider({
+        mnemonic: {
+          phrase: mnemonic
+        },
+        providerOrUrl:  `https://matic-mumbai.chainstacklabs.com`,
+        chainId: 80001
+      }),
+      network_id: 80001,
+      confirmations: 2,
+      timeoutBlocks: 200,
+      skipDryRun: true,
+      chainId: 80001
+    }
   }
 };
