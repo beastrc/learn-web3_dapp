9ae0ea18d63c288786d6a90af10efaf8873177d9
beastrc <makarov0114@outlook.com>
2021-08-31 18:09:49 +0200

    add back google font

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,53 +10,6 @@ class MyDocument extends Document {
   render() {
     return (
       <Html>
-        <Head />
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
-  }
-}
-
-export default MyDocument;
-
-
-/*
-import Document, { Html, Head, Main, NextScript } from 'next/document'
-import { ServerStyleSheet } from 'styled-components'
-
-export default class MyDocument extends Document {
-	static async getInitialProps(ctx: any) {
-		const sheet = new ServerStyleSheet()
-		const originalRenderPage = ctx.renderPage
-
-		try {
-			ctx.renderPage = () =>
-				originalRenderPage({
-					enhanceApp: (App:any) => (props: any) =>
-						sheet.collectStyles(<App {...props} />),
-				})
-
-			const initialProps = await Document.getInitialProps(ctx)
-			return {
-				...initialProps,
-				styles: (
-					<>
-						{initialProps.styles}
-						{sheet.getStyleElement()}
-					</>
-				),
-			}
-		} finally {
-			sheet.seal()
-		}
-	}
-
-	render() {
-    return (
-      <Html>
         <Head>
 		    <link
                 href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap"
@@ -68,7 +21,8 @@ export default class MyDocument extends Document {
           <NextScript />
         </body>
       </Html>
-		)
-	}
+    );
+  }
 }
-*/
+
+export default MyDocument;
