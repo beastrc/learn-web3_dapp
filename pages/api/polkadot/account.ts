872b89ffa8f6a50c822bc328bf5bc557882a8778
beastrc <makarov0114@outlook.com>
2021-09-08 11:29:52 +0200

    Update account.ts

--- pages/api/polkadot/account.ts
+++ pages/api/polkadot/account.ts
@@ -5,29 +5,30 @@ import { Keyring } from '@polkadot/api';
 import { mnemonicGenerate, mnemonicValidate } from '@polkadot/util-crypto';
 
 export default async function account(
-	_req: NextApiRequest,
-	res: NextApiResponse<PolkadotAccountResponse | string>
+    _req: NextApiRequest,
+    res: NextApiResponse<PolkadotAccountResponse | string>
   ) {
-    const keyring = new Keyring({ type: "sr25519" });
+    try {        
+        const keyring = new Keyring({ type: "sr25519" });
 
-    // Create mnemonic string
-    const mnemonic = undefined;
+        // Create mnemonic string
+        const mnemonic = undefined;
 
-    const isValidMnemonic = mnemonicValidate(mnemonic);
-    if (!isValidMnemonic) {
-      throw Error('Invalid Mnemonic')
-    }
+        const isValidMnemonic = mnemonicValidate(mnemonic);
+        if (!isValidMnemonic) {
+          throw Error('Invalid Mnemonic')
+        }
 
-    // Add an account derived from the mnemonic
-    const account = undefined;
-    const address = undefined;
-    const jsonWallet = undefined;
-    res.status(200).json({
-      address,
-      mnemonic,
-      jsonWallet,
-    });
-  } catch (error) {
-    res.status(500).json("Account creation failed")
+        // Add an account derived from the mnemonic
+        const account = undefined;
+        const address = undefined;
+        const jsonWallet = undefined;
+        res.status(200).json({
+          address,
+          mnemonic,
+          jsonWallet,
+        });
+    } catch (error) {
+        res.status(500).json("Account creation failed")
   }
 }
