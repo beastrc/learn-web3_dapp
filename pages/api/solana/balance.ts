9c23f9876d7eb3defe8065776721bd276fbb1497
beastrc <makarov0114@outlook.com>
2021-09-11 18:53:54 +0200

    Update balance.ts

--- pages/api/solana/balance.ts
+++ pages/api/solana/balance.ts
@@ -12,6 +12,9 @@ export default async function balance(
     const connection = new Connection(url, 'confirmed');
     const publicKey = undefined;
     const balance = undefined;
+    if (balance === 0 || balance === undefined) {
+      throw new Error('Account not funded');
+    }
     res.status(200).json(balance);
   } catch (error) {
     res.status(500).json(error.message);
