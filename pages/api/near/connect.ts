53666ddfaf1e960c31dbdd866a269076f41d0471
beastrc <makarov0114@outlook.com>
2021-10-19 08:11:02 +0200

    Update connect.ts

--- pages/api/near/connect.ts
+++ pages/api/near/connect.ts
@@ -6,15 +6,15 @@ export default async function connection(
   req: NextApiRequest,
   res: NextApiResponse<string>,
 ) {
-  const {network} = req.body;
+  const {NETWORK} = req.body;
   try {
-    const config = configFromNetwork(network);
+    const config = configFromNetwork(NETWORK);
     const near = undefined;
     const provider = undefined;
     const status = undefined;
     return res.status(200).json(status.version.version);
   } catch (error) {
-    console.error(error);
-    return res.status(500).json('Error connection to NEAR');
+    let errorMessage = error instanceof Error ? error.message : 'Unknown Error';
+    return res.status(500).json(errorMessage);
   }
 }
