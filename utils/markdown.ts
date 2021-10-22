28b749f42dd09d6e9a115e47c28ffa20ec62aa47
beastrc <makarov0114@outlook.com>
2021-10-22 19:51:47 +0000

    Add preface (step 0) to every pathway

--- utils/markdown.ts
+++ utils/markdown.ts
@@ -9,7 +9,11 @@ export function fetchMarkdownForChainId(chainId: CHAINS): MarkdownForChainIdT {
   const steps = CHAINS_CONFIG[chainId].steps.map((step) => step.id);
 
   return steps.reduce((markdownMap, stepId) => {
-    const filePath = path.join(MARKDOWN_PATH, chainId, `${stepId}.md`);
+    // We have a special case for preface because it's common to every pathway
+    const filePath =
+      stepId === PROTOCOL_STEPS_ID.PREFACE
+        ? path.join(MARKDOWN_PATH, `PREFACE.md`)
+        : path.join(MARKDOWN_PATH, chainId, `${stepId}.md`);
     const fileContent = fs.readFileSync(filePath, 'utf-8');
     const markdown = fileContent.toString();
     markdownMap[stepId as PROTOCOL_STEPS_ID] = markdown;
