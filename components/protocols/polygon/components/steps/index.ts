8752de45cb1f437e0bd206d1b4d36fab87d54970
beastrc <makarov0114@outlook.com>
2021-10-22 14:31:30 +0200

    remove deploy component for polygon

--- components/protocols/polygon/components/steps/index.ts
+++ components/protocols/polygon/components/steps/index.ts
@@ -3,8 +3,7 @@ import Query from '@figment-polygon/components/steps/Query';
 import Restore from '@figment-polygon/components/steps/Restore';
 import Balance from '@figment-polygon/components/steps/Balance';
 import Transfer from '@figment-polygon/components/steps/Transfer';
-import Deploy from '@figment-polygon/components/steps/Deploy';
 import Setter from '@figment-polygon/components/steps/Setter';
 import Getter from '@figment-polygon/components/steps/Getter';
 
-export {Balance, Connect, Deploy, Query, Restore, Setter, Transfer, Getter};
+export {Balance, Connect, Query, Restore, Setter, Transfer, Getter};
