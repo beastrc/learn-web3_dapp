51b932e5678ac240d1d1954acd4f6322704a7ce0
beastrc <makarov0114@outlook.com>
2021-12-02 21:16:32 +0100

    update step order

--- components/protocols/polygon/index.tsx
+++ components/protocols/polygon/index.tsx
@@ -24,8 +24,8 @@ const Polygon: React.FC = () => {
       {stepId === PROTOCOL_STEPS_ID.GET_BALANCE && <Steps.Balance />}
       {stepId === PROTOCOL_STEPS_ID.TRANSFER_TOKEN && <Steps.Transfer />}
       {stepId === PROTOCOL_STEPS_ID.DEPLOY_CONTRACT && <Steps.Deploy />}
-      {stepId === PROTOCOL_STEPS_ID.SET_CONTRACT_VALUE && <Steps.Setter />}
       {stepId === PROTOCOL_STEPS_ID.GET_CONTRACT_VALUE && <Steps.Getter />}
+      {stepId === PROTOCOL_STEPS_ID.SET_CONTRACT_VALUE && <Steps.Setter />}
       {stepId === PROTOCOL_STEPS_ID.RESTORE_ACCOUNT && <Steps.Restore />}
     </>
   );
